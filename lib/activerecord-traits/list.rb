657292951f9dbbd5ff6b86e901a46c0d76ca19a2
Yaroslav Konoplov <yaroslav@inbox.com>
2015-12-14 22:13:54 +0200

    List

---
+++ lib/activerecord-traits/list.rb
@@ -0,0 +1,52 @@
+module Traits
+  class List
+    include Enumerable
+
+    def initialize(list = [])
+      @list = list
+    end
+
+    def filter(hash)
+      select do |attr|
+        hash.all? do |method, expected|
+          returned = attr.send(method)
+          if expected.is_a?(Array)
+            expected.include?(returned)
+          else
+            returned == expected
+          end
+        end
+      end
+    end
+
+    def first_where(hash)
+      find do |attr|
+        hash.all? do |method, expected|
+          returned = attr.send(method)
+          if expected.is_a?(Array)
+            expected.include?(returned)
+          else
+            returned == expected
+          end
+        end
+      end
+    end
+
+    def [](arg)
+      by_name(arg)
+    end
+
+    def each(&block)
+      @list.each(&block)
+    end
+
+    def by_name(name)
+      name = name.to_s.to_sym
+      find { |attr| attr.name == name }
+    end
+
+    def to_hash
+      each_with_object({}) { |item, memo| memo[item.name] = item.to_hash }
+    end
+  end
+end
