19a07247812ca443fa5830c9e7b798e9f19de0ee
Yaroslav Konoplov <yaroslav@inbox.com>
2015-12-14 19:45:38 +0200

    Attribute: Naming

---
+++ lib/activerecord-traits/concerns/attribute/naming.rb
@@ -0,0 +1,19 @@
+module Traits
+  class Attribute
+    module Naming
+      extend ActiveSupport::Concern
+
+      def name
+        column.name.to_sym
+      end
+
+      def quoted_name
+        model.klass.connection.quote_column_name(name)
+      end
+
+      def to_hash
+        super.merge!(name: name, quoted_name: quoted_name)
+      end
+    end
+  end
+end
