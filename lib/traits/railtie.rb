6143413d426cb13e6837cbba3233f5cda7a7bac0
Yaroslav Konoplov <eahome00@gmail.com>
2016-10-20 16:19:10 +0300

    Change Engine to Railtie

--- lib/traits/railtie.rb
+++ lib/traits/railtie.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 module Traits
-  class Engine < Rails::Engine
-    isolate_namespace Traits
-
-    config.before_initialize do |app|
+  class Railtie < Rails::Railtie
+    initializer do |app|
       unless app.config.cache_classes
         ActionDispatch::Reloader.to_prepare do
           Traits.invalidate_loaded_active_record_descendants!
